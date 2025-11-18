063928bc819a1861b8964d1a2e04f955d65f839d
Jay_Dallaway <jaydallaway@outlook.com>
2025-11-18 17:08:45 +0000

    upload connectdb.php to main

---
+++ connectdb.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_host = 'localhost';
+$db_name = 'cs2team38_db';
+$username = 'root';
+$password = '';
+
+try {
+	$db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password); 
+	#$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $ex) {
+	echo("Failed to connect to the database.<br>");
+	echo("error in connectdb" . $ex->getMessage());
+	exit;
+}
+
+?>
