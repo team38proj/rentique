1c37d40d5f712eda4f36f1129ca7994423cfa9d2
Nisah Ahmed <240119331@aston.ac.uk>
2025-11-27 15:51:58 +0000

    AboutUs.php

---
+++ AboutUs.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rentique | About Us</title>
+    <link rel="stylesheet" href="rentique.css">
+    <link rel="icon" type="image/png" href="logo.png">
+</head>
+<body id="aboutPage">
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo">
+                <img src="logo.png">
+                <span>rentique.</span>
+            </div>
+            <ul class="nav-links">
+                <li><a href="Homepage.html">Home</a></li>
+                <li><a href="index.html">Shop</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#" class="btn login">Login</a></li>
+                <li><a href="#" class="btn signup">Sign Up</a></li>
+            </ul>
+        </nav>
+
+
+    </header>
+
+    <img src="rentiquebanner2.png" width= "1255">
+
+	<div class= "about-us">
+	<div class= "about-text">
+		<h2>About Us</h2>
+		<h3>Welcome to Rentique</h3>
+	
+		
+		<p>Rentique: Project Dakar
+		
+		<br> 
+		<br>
+		At Rentique we hold the belief that style must not be sacrificed at the cost of our environment or society. Located in Dakar, Senegal, among one 
+of the globe’s most contaminated urban areas. Our goal is to combat the ecological damage inflicted by fast fashion by providing an eco-friendly 
+option. 
+<br>
+<br>
+Rentique is an fashion rental service aimed at minimizing waste by prolonging the durability of clothes and providing access, to premium 
+fashion for all. Rather than purchasing clothes for single use, clients have the option to rent items and then return them with ease. Rentique 
+handles everything, from shipping to expert cleaning, ensuring fashion is hassle-free.Our website offers a range of designer clothes, vintage finds,
+standout accessories and footwear. Through category filters and tailored user profiles, Rentique simplifies the process of dressing stylishly while
+ supporting ethical decisions. 
+ <br>
+ <br>
+ Fast fashion is responsible, for 8–10% of carbon emissions and Dakar experiences this effect firsthand. As Group 38 
+ we chose the name Rentique to honor Dakar's pollution status and emphasize our dedication to helping resolve the issue. 
+ <br>
+ <br>
+ Moreover our efforts extend beyond sustainability.
+To support communities impacted by damage Rentique contributes 5% of all earnings to organizations that help to minimize waste and enhance social 
+welfare. Each rental not only lessens fashion waste but also contributes to beneficial transformation in Dakar.
+</p>
+	</div>
+		<div class= "about-image">
+		<img src= "map.png">
+		</div>
+	</div>	
+		
+	
+	
+	
+    <footer>
+        <p>© 2025 Rentique. All rights reserved.</p>
+    </footer>
+</body>
+</html>
