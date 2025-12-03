4e4475ea2a78f85e2fde2620fe0eae0281f23167
oliwelsh21 <welsho29@gmail.com>
2025-12-03 16:48:26 +0000

    Add basket icon link to navigation

--- Contact.php
+++ Contact.php
@@ -29,6 +29,7 @@ require_once 'connectdb.php';
                 <li><a href="Contact.php">Contact</a></li>
                 <li><a href="login.php" class="btn login">Login</a></li>
                 <li><a href="signup.html" class="btn signup">Sign Up</a></li>
+                <li><a href="basketPage.php" class="cart-icon"><img src="basket.png" alt="Basket"></a></li>
             </ul>
         </nav>
     </header>
