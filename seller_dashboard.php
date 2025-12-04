703a4172a6e781d7a18195023c6b09633554854e
oliwelsh21 <welsho29@gmail.com>
2025-12-04 14:12:58 +0000

    Update navigation links in seller dashboard

--- seller_dashboard.php
+++ seller_dashboard.php
@@ -27,9 +27,12 @@
             <ul class="nav-links">
                 <li><a href="#">Home</a></li>
                 <li><a href="#shop">Shop</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href="Homepage.html">Home</a></li>
+                <li><a href="productsPage.php">Shop</a></li>
+                <li><a href="AboutUs.php">About</a></li>
+                <li><a href="Contact.php">Contact</a></li>
                 <li><a href="auth_login.html" class="btn logout">Logout</a></li>
+                <li><a href="basketPage.php" class="cart-icon"><img src="basket.png" alt="Basket"></a></li> 
             </ul>
         </nav>
     </header>
