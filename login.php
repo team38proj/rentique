a03d7e0fc2a4e94b64e38cd1f25b00ea4286bc13
Jay_Dallaway <jaydallaway@outlook.com>
2025-12-05 10:09:02 +0000

    Debugged Login and Signup Pages
    Fixed login and signup pages to allow webpages to successfully display contents

--- login.php
+++ login.php
@@ -1,6 +1,3 @@
-/*
->>>> Login page
-*/
 <?php
 
 /*
@@ -43,5 +40,81 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 }
-$csrf_token = generate_csrf_token();
+// $csrf_token = generate_csrf_token();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+<!--Saja - backend (toggleable theme)-->
+<script>
+document.addEventListener("DOMContentLoaded", () => {
+    const theme = localStorage.getItem("theme") || "light";
+    if (theme === "dark") document.body.classList.add("dark-mode");
+});
+</script>
+    
+    <link rel="stylesheet" href="rentique.css">    
+    <link rel="stylesheet" href="style.css">
+    <script src="login.php" defer></script>
+    
+</head>
+<body>
+    <div class="auth-wrapper">
+<div class="login-container">
+    
+    <h2>Login</h2>
+
+    <form id="loginform" novalidate>
+
+        
+        <div class="field">
+            <div class="icon">
+                
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
+                    <path d="M160-200q-33 0-56.5-23.5T80-280v-400q0-33 23.5-56.5T160-760h640q33 0 56.5 23.5T880-680v400q0 33-23.5 56.5T800-200H160Zm320-260L160-640v360h640v-360L480-460Z"/>
+                </svg>
+            </div>
+            <input type="email" id="email" placeholder="Email">
+        </div>
+
+        
+        <div class="field">
+            <div class="icon">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
+                    <path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm240-200q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80Z"/>
+                </svg>
+            </div>
+            <input type="password" id="password" placeholder="Password">
+        </div>
+
+
+        <button type="submit" class="btn">Login</button>
+    </form>
+
+    <p id="error-message"></p>
+    <p>New here? <a href="signup.php">Create an Account</a></p>
+
+</div>
+</div>
+
+<!--Saja - backend (toggleable theme)-->
+<script>
+const toggleBtn = document.getElementById("theme-toggle");
+
+if (toggleBtn) {
+    toggleBtn.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+        localStorage.setItem("theme", 
+            document.body.classList.contains("dark-mode") ? "dark" : "light"
+        );
+    });
+}
+</script>
+
+</body>
+</html>
